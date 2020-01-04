9ab2dd2badeeaf22161f0806d32b268a7297646b
Jishan Sharif <jishanmsharif@gmail.com>
2020-01-04 18:46:08 +0400

    Edited my while loop condition

--- word_count.py
+++ word_count.py
@@ -5,11 +5,19 @@ Created on Sun Dec 29 18:56:12 2019
 
 @author: jishanmsharif
 """
+
+#f = open("newfile.txt","w+")
+#for i in range(10):
+    #print("This is line \n" (i +1))
+
+#How to use a dictionary in this scenario?
 #Built in input function allows users to interact with the program
-text = str(input("Enter text here"))
 #str converts the added text into a string
 loop = "Yes"
 while loop == "Yes":
+    text = str(input("Enter text here"))
+    if text == "x":
+        break 
 #any unique charecter is converted to an empty space
 #This is so that the program doesn't count these charecters as a separate word
     for char in '-,.\n':
@@ -21,4 +29,7 @@ while loop == "Yes":
     length_list = len(text_list)
 #we can now calculate the length of the new list
     print(length_list)
-    loop = input("Enter new text here, Type \"Yes\" to continue")
+    
+
+
+#newdict = {"Mom":"Yesmin", "Dad":"Delawar", "Favorite Son": "Jishan"}
